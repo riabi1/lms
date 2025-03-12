94205b53f9439b6ccc6b99274222576e847b211c
kanzarieya <kanzarie04@gmail.com>
2025-03-13 00:57:01 +0100

    sidebar

--- resources/views/instructor/body/sidebar.blade.php
+++ resources/views/instructor/body/sidebar.blade.php
@@ -1,5 +1,4 @@
-
-
+<!-- resources/views/instructor/body/sidebar.blade.php -->
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
@@ -11,106 +10,82 @@
                 $instructor = Auth::guard('instructor')->user();
                 $status = $instructor->status ?? null;
             @endphp
+            @if ($instructor)
+                <small>Status: 
+                    @if ($status == 1)
+                        <span class="badge bg-success">Active</span>
+                    @else
+                        <span class="badge bg-danger">Inactive</span>
+                    @endif
+                </small>
+            @endif
         </div>
-        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
-        </div>
-     </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i></div>
+    </div>
     <!--navigation-->
     <ul class="metismenu" id="menu">
-        
-       <li>
-    <a href="{{ route('instructor.dashboard') }}">
-        <div class="parent-icon"><i class='bx bx-home-alt'></i></div>
-        <div class="menu-title">Dashboard</div>
-    </a>
-</li>
+        <li>
+            <a href="{{ route('instructor.dashboard') }}">
+                <div class="parent-icon"><i class='bx bx-home-alt'></i></div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
         @if ($status == 1)
-
-        <li class="menu-label">Course Manage </li>
-       
-        <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class='bx bx-cart'></i>
-                </div>
-                <div class="menu-title">Course Manage</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>All Course </a>
-                </li>
-                
-                
-            </ul>
-        </li>
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">All Orders</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>All Orders</a>
-                </li>
-                
-               
-            </ul>
-        </li>
-
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">All Question</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>All Question</a>
-                </li>
-                
-               
-            </ul>
-        </li>
-
-
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">Manage Coupon</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>All Coupon</a>
-                </li>
-                
-               
-            </ul>
-        </li>
-
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">Manage Reivew</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>All Review</a>
-                </li>
-                
-               
-            </ul>
-        </li>
-
-        <li>
-            <a class="has-arrow" href="javascript:;">
-                <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                </div>
-                <div class="menu-title">Live Chat</div>
-            </a>
-            <ul>
-                <li> <a href="#"><i class='bx bx-radio-circle'></i>Live Chat</a>
-                </li>
-                
-               
-            </ul>
-        </li>
+            <li class="menu-label">Course Manage</li>
+            <li>
+                <a href="javascript:;" class="has-arrow">
+                    <div class="parent-icon"><i class='bx bx-cart'></i></div>
+                    <div class="menu-title">Course Manage</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>All Course</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i></div>
+                    <div class="menu-title">All Orders</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>All Orders</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i></div>
+                    <div class="menu-title">All Question</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>All Question</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i></div>
+                    <div class="menu-title">Manage Coupon</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>All Coupon</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i></div>
+                    <div class="menu-title">Manage Review</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>All Review</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow" href="javascript:;">
+                    <div class="parent-icon"><i class='bx bx-bookmark-heart'></i></div>
+                    <div class="menu-title">Live Chat</div>
+                </a>
+                <ul class="mm-collapse">
+                    <li><a href="#"><i class='bx bx-radio-circle'></i>Live Chat</a></li>
+                </ul>
+            </li>
         @endif
     </ul>
     <!--end navigation-->
