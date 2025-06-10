4949f2f8b4d35bc883deab62d70c5375d4f14bf3
kanzarieya <kanzarie04@gmail.com>
2025-06-10 18:09:08 +0100

    fix

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -30,26 +30,7 @@ $blogCategories = BlogCategory::orderBy('name', 'ASC')->get();
         <!-- Theme Picker & Auth Links -->
         <div class="col-lg-6">
           <div class="header-widget d-flex flex-wrap align-items-center justify-content-end">
-            <div class="theme-picker d-flex align-items-center mrpline-block">
-              <button class="theme-picker-btn dark-mode-btn" title="Dark mode">
-                <svg id="moon" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                </svg>
-              </button>
-              <button class="theme-picker-btn light-mode-btn" title="Light mode">
-                <svg id="sun" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                  <circle cx="12" cy="12" r="5"></circle>
-                  <line x1="12" y1="1" x2="12" y2="3"></line>
-                  <line x1="12" y1="21" x2="12" y2="23"></line>
-                  <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                  <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                  <line x1="1" y1="12" x2="3" y2="12"></line>
-                  <line x1="21" y1="12" x2="23" y2="12"></line>
-                  <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                  <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                </svg>
-              </button>
-            </div>
+
             <ul class="generic-list-item d-flex flex-wrap align-items-center fs-14 header-top-list border-left border-left-gray pl-4 ml-4">
               @guest
               <li class="d-flex align-items-center pr-4 mr-4 border-right border-right-gray">
